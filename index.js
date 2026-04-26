8e08a2a8bced1de798c6729ce61859b98edcacf3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-26 15:04:37 +1000

    Initial commit with the basic input UI.

---
+++ index.js
@@ -0,0 +1,97 @@
+import * as gesel from "./node_modules/gesel/src/index.js";
+
+const proxy = "https://cors-proxy.aaron-lun.workers.dev";
+const ref_url = "https://github.com/gesel-inc/feedstock/releases/download/indices-v0.3.0";
+const gene_url = "https://github.com/gesel-inc/feedstock/releases/download/genes-v0.3.0";
+const ref_key = ref_url.substr(ref_url.lastIndexOf("/") + 1);
+const gene_key = gene_url.substr(gene_url.lastIndexOf("/") + 1);
+
+async function getFile(url, key, file) {
+    const cache = await caches.open(key);
+    const address = proxy + "/" + encodeURIComponent(url + "/" + file);
+
+    let existing = await cache.match(address);
+    if (typeof existing == "undefined") {
+        existing = await fetch(address); 
+        cache.put(address, existing.clone());
+    }
+
+    if (!existing.ok) {
+        throw new Error("failed to request '" + file + "' (status " + String(existing.status) + ")");
+    }
+    return existing.arrayBuffer();
+}
+
+const config = gesel.newConfig(
+    /* fetchGeneFile = */ file => getFile(gene_url, gene_key, file),
+    /* fetchDatabaseFile = */ file => getFile(ref_url, ref_key, file),
+    /* fetchDatabaseRanges = */ async (file, start, end) => {
+        let address = proxy + "/" + encodeURIComponent(ref_url + "/" + file);
+        const everything = [];
+        // GitHub doesn't support multi-range requests, but if it did, we could use that instead.
+        for (var i = 0; i < start.length; i++) {
+            const resp = fetch(full_url, { headers: { Range: "bytes=" + String(start[i]) + "-" + String(end[i] - 1) } })
+                .then(
+                    res => {
+                        if (!res.ok) {
+                            throw new Error("failed to fetch range from '" + file + "' (status " + String(res.status) + ")");
+                        }
+                        return res.arrayBuffer();
+                    }
+                );
+            everything.push(resp);
+        }
+        return await Promise.all(everything);
+    }
+);
+
+/*****************************************************/
+
+const cached_collections = { "chosen": null };
+
+async function checkCollections() {
+    const all_species = document.getElementById("species-list").childNodes;
+    let chosen_species = null; 
+    for (const node of all_species) {
+        if (node.nodeName == "INPUT" && node.getAttribute("name") == "species" && node.checked) {
+            chosen_species = node.getAttribute("value");
+            break;
+        }
+    }
+
+    if (chosen_species == null || cached_collections.chosen == chosen_species) {
+        return true;
+    }
+
+    const all_collections = await gesel.fetchAllCollections(chosen_species, config);
+
+    let collection_elements = [];
+    for (let i = 0; i < all_collections.length; i++) {
+        const check = document.createElement("input");
+        check.setAttribute("type", "checkbox");
+        const id = "collection-" + String(i);
+        check.setAttribute("id", id);
+        check.setAttribute("name", "filter-collections");
+        check.setAttribute("value", String(i));
+        check.setAttribute("checked", "");
+        collection_elements.push(check);
+
+        const lab = document.createElement("label");
+        lab.setAttribute("for", id);
+        lab.innerText = all_collections[i].title;
+        collection_elements.push(lab);
+
+        collection_elements.push(document.createElement("br"));
+    }
+
+    const coldiv = document.getElementById("collection-availability");
+    coldiv.replaceChildren(...collection_elements);
+    cached_collections.chosen = chosen_species;
+
+    return true;
+}
+
+checkCollections();
+
+window.checkCollections = checkCollections;
+
